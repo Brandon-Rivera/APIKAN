23c257b5e442a5c45677f78333173d8b87ec73db
Roy-maker <carsanchezsie@gmail.com>
2023-04-10 16:18:48 -0600

    API v2.1

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ app.use(express.json());
 app.use('/login', login);
 app.use('/test', test);
 
+app.get('/roy', async (req, res) => {
+    console.log('body: ', req.body);
+    const values = {
+        email: req.body.email, 
+        pass: req.body.pass
+    };
+
+    const response = await fetch(`https://university6y.kanbanize.com/api/v2/workspaces//format/json`,
+    {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(values)
+    });
+    const data = await response.json();
+    res.json(data);
+})
+
 app.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`)
 });
