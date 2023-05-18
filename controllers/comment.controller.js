18f95b33a66ff1f15a7fde1dc34b4a411f906226
Roy-maker <carsanchezsie@gmail.com>
2023-05-17 18:28:09 -0600

    API v8.7 Receives file array on comment

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -5,11 +5,14 @@ const requestBuilder = require('../helpers/requestBuilder');
 
 //receives post request
 //redirects to post request
-//requires body with domain, apikey, cardid and comment
+//requires body with domain, apikey, cardid, comment and files
+//*Important note: files is an array of objects composed of a
+//file_name(string) and link(string) attributes
 module.exports.doComment = async (req, res) => 
 {
     const values = {
-        text: req.body.comment
+        text: req.body.comment,
+        attachments_to_add: req.body.files
     };
     const response = await fetch(`https://${req.body.domain}.kanbanize.com/api/v2/cards/${req.body.cardid}/comments`,
     {
